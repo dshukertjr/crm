09cc7d2e683aa8fc5558f37147fba35e038ca631
Hub Tyler <18113850+dshukertjr@users.noreply.github.com>
2021-06-18 13:26:47 +0900

    Fixed login page design

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
+import React from 'react'
 import { ReactElement } from 'react'
+import { supabase } from '../lib/api'
+import Link from 'next/link'
 
 export default function LoginPage(): ReactElement {
+  const login = async (): Promise<void> => {
+    const email = ''
+    const password = ''
+    await supabase.auth.signIn({
+      email,
+      password,
+    })
+  }
   return (
     <div>
       <Head>
@@ -9,7 +20,75 @@ export default function LoginPage(): ReactElement {
         <meta name="description" content="Log into your simple CRM" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>login page</div>
+
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={login}>
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="text-sm">
+              <Link href="/forgot-password">
+                <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Forgot your password?
+                </a>
+              </Link>
+            </div>
+
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md transition-colors text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md transition-colors text-indigo-600 hover:bg-indigo-200 border-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
